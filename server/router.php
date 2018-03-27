1513e7054269a037b360b89f34005fa3cfbaf9ab
Daniel de Andrade Varela <professor.dav@gmail.com>
2018-03-27 12:03:01 -0300

    ajax

--- server/router.php
+++ server/router.php
@@ -1,6 +1,13 @@
 <?php
 
-// print_r($_SERVER);
+header('Content-Type: application/json;charset=UTF-8');
+header('Accept: application/json');
+header("Access-Control-Allow-Origin: *");
+
+sleep(3);
+
+$content = json_decode( file_get_contents("php://input"), true );
+echo json_encode($content);
 
 // $folder = $_SERVER['SCRIPT_NAME'];
 // $method = strtolower($_SERVER['REQUEST_METHOD']);
@@ -9,5 +16,5 @@
 // $handle = include ".".$folder.".php";
 // $handle->$method($param);
 
-$db = include "./framework/Connection.php";
-$db->select();
+// $db = include "./framework/Connection.php";
+// $db->select();
